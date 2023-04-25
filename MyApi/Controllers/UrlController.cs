cdae6ea6598320e2e818e77782ba211a3872334b
mipoof <kiki.ket.key@gmail.com>
2023-04-25 17:07:55 +0800

    KV系統

---
+++ MyApi/Controllers/UrlController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApi.Controllers;
+
+
+
+[ApiController]
+[Route("[controller]")]
+public class UrlController : ControllerBase
+{
+    // GET
+    public IEnumerable<string> Get()
+    {
+
+        IList<string> urls = new List<string>();
+        urls.Add("http://google");
+        urls.Add("http://youtube");
+        
+        return urls.ToArray();
+    }
+}
